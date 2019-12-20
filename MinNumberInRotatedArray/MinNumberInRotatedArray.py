99c52193038f99c11dd6f6fb03f9ba6ae21bd909
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-20 14:28:16 +0800

    Update MinNumberInRotatedArray.py

--- MinNumberInRotatedArray/MinNumberInRotatedArray.py
+++ MinNumberInRotatedArray/MinNumberInRotatedArray.py
@@ -1,1 +1,39 @@
-
+## Python Solution 1:
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n==0:
+            return 0
+        res = nums[0]
+        for i in range (n):
+            if res >nums[i]:
+                res = nums[i]
+        return res
+## Python Solution 2:
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n==0:
+            return 0
+        res = nums[0]
+        start = 0
+        end = n-1
+        while end>=start:
+            mid = start+(end-start)//2
+            if nums[mid]>=res:
+                start=mid+1
+            if nums[mid]<res:
+                end = mid-1
+            res = min(res,nums[mid])
+        return res
+## Python Solution 3:
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n==0:
+            return 0
+        res = nums[0]
+        for i in range (n):
+            if res >nums[i]:
+                res = nums[i]
+        return res
