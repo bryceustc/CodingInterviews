204130ed886f2cbad1a479652b7af6272fd77bec
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-23 14:33:45 +0800

    Update PrintMatrix.cpp

--- PrintMatrix/PrintMatrix.cpp
+++ PrintMatrix/PrintMatrix.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    vector<int> printMatrix(vector<vector<int>> matrix) {
+        vector<int> res;
+        if (matrix.empty()) return res;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int u = 0;
+        int d = m-1;
+        int l = 0;
+        int r = n-1;
+        while(true)
+        {
+          for (int i=l;i<=r;i++) res.push_back(matrix[u][i]);//向右移动至最右
+          if (++u > d) break;//重新定义上边界
+          for (int i=u;i<=d;i++) res.push_back(matrix[i][r]);//向下
+          if (--r < l) break;//重新定义右边界
+          for (int i=r;i>=l;i--) res.push_back(matrix[d][i]);//向左
+          if (--d < u) break;//重新定义下边界
+          for (int i=d;i>=u;i--) res.push_back(matrix[i][l]);//向上
+          if (++l > r) break;//重新定义左边界
+        }
+        return res;
+    }
+};
+
+
+int main()
+{
+    vector<vector<int>> nums = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    vector<vector<int>> res;
+    res = Solution().printMatrix(nums);
+    for (int i=0;i<res.size()i++)
+    {
+        cout<< res[i] <<endl;
+    }
+    system("pause");
+    return 0;
+}
