c358e1b1bb874c2148d182e60b29939a11e2f1d2
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-12 14:47:08 +0800

    Update MissingNumber.py

--- MissingNumber/MissingNumber.py
+++ MissingNumber/MissingNumber.py
@@ -1,1 +1,42 @@
-
+### Python Solution 1:
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = 0
+        n = len(nums)
+        nums_set = set(nums)
+        for num in range(n+1):
+            if num not in nums_set:
+                return num
+        return res
+### Python Solution 2:
+# -*- coding:utf-8 -*-
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = 0;
+        n = len(nums)
+        x = range(n+1)
+        x = list(x)           ### Python3 需要用list转换成数组
+        res = sum(x)-sum(nums)
+        return res
+      
+      
+ # -*- coding:utf-8 -*-
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = n
+        for i in range(n):
+            res = res ^ nums[i]
+            res = res ^ i
+        return res
+### Python Solution 3:
+# -*- coding:utf-8 -*-
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        n = len(nums)
+        if nums[-1] != n:
+            return n
+        for i in range(0,n):
+            if i!=nums[i]:
+                return i
