d815329666f9f9eb99ae2c567ba0509b1c92e868
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-15 15:44:25 +0800

    Update ContinousCards.cpp

--- ContinousCards/ContinousCards.cpp
+++ ContinousCards/ContinousCards.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    bool IsContinuous( vector<int> nums ) {
+        int n = nums.size();
+        if (nums.empty()|| n!=5)
+            return false;
+        sort(nums.begin(),nums.end());
+        int numZero = 0;
+        int gap = 0;
+        for (int i=0;i<n;i++)
+        {
+            if (nums[i]==0)
+                numZero++;
+        }
+        for (int i=numZero, j=numZero+1;j<n;i++,j++)
+        {
+            if (nums[i]==nums[j]) return false;
+            gap += nums[j]-nums[i]-1;
+        }
+        if (numZero<gap) return false;
+        return true;
+    }
+};
