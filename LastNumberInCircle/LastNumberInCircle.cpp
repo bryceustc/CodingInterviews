c335ddc9555b4823e8c729aa073f716b0c8e3af7
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-16 15:58:44 +0800

    Update LastNumberInCircle.cpp

--- LastNumberInCircle/LastNumberInCircle.cpp
+++ LastNumberInCircle/LastNumberInCircle.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int LastRemaining_Solution(int n, int m)
+    {
+        int res =-1;
+        if (n<=0 || m<0)
+            return res;
+        vector<int> nums;
+        for (int i=0;i<=n;i++)
+        {
+            nums.push_back(i);
+        }
+        int start =0;
+        while(nums.size()>1)
+        {
+            int end = (start+m-1)%(nums.size());
+            nums.erase(nums.begin()+end);
+            start = end;
+        }
+        res = nums[0];
+        return res;
+    }
+};
