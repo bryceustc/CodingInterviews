77179e9d498d890c061895778f986a25697c84a7
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-29 20:09:39 +0800

    Update StringPermutation.py

--- StringPermutation/StringPermutation.py
+++ StringPermutation/StringPermutation.py
@@ -25,3 +25,26 @@ class Solution:
             self.DFS(s,level+1,visited,out,res)
             out = out[:-1]
             visited[i]=0
+### Solution 2:
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.result = []
+    def Permutation(self, ss):
+        # write code here
+        if len(ss) == 0:
+            return []
+        self.PermutationCore(ss, 0)
+        sorted(self.result)
+        return self.result
+    def PermutationCore(self, str_, begin):
+        if begin == len(str_):
+            self.result.append(str_)
+            return
+        for i in range(begin, len(str_)):
+            if i != begin and str_[i] == str_[begin]:
+                continue
+            str_list = list(str_)
+            str_list[i], str_list[begin] = str_list[begin], str_list[i]
+            str_ = ''.join(str_list)
+            self.PermutationCore(str_, begin+1)
