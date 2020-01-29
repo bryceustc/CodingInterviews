a2c91168bfbe0da041c061aede8ad6b34c766db1
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-29 20:10:09 +0800

    Update StringPermutation.cpp

--- StringPermutation/StringPermutation.cpp
+++ StringPermutation/StringPermutation.cpp
@@ -38,3 +38,42 @@ public:
         }
     }
 };
+
+
+// C++ Solution 2:
+class Solution 
+{
+public:
+    vector<string> Permutation(string str) {
+        //判断输入
+        if(str.length() == 0){
+            return result;
+        }
+        PermutationCore(str, 0);
+        //对结果进行排序
+        sort(result.begin(), result.end());
+        return result;
+    }
+    
+private:
+    void PermutationCore(string str, int begin){
+        //递归结束的条件：第一位和最后一位交换完成
+        int n = str.size();
+        if(begin == n)
+        {
+            result.push_back(str);
+            return;
+        }
+        for(int i = begin; i < n; i++){
+            //如果字符串相同，则不交换
+            if(i != begin && str[i] == str[begin]){
+                continue;
+            }
+            //位置交换
+            swap(str[begin], str[i]);
+            //递归调用，前面begin+1的位置不变，后面的字符串全排列
+            PermutationCore(str, begin + 1);
+        }
+    }
+    vector<string> result;
+};
