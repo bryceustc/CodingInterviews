3c4e0769cc27d6bec9cd595c243085b183f85c3c
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-31 20:31:27 +0800

    Update NumberOf1.py

--- NumberOf1/NumberOf1.py
+++ NumberOf1/NumberOf1.py
@@ -1,1 +1,39 @@
-
+# Solution 1:
+# -*- coding:utf-8 -*-
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        res = 0
+        for i in range(1,n+1):
+            res += self.NumberOf1(i)
+        return res
+    def NumberOf1(self,n):
+        num = 0
+        while n>0:
+            if n%10==1:
+                num+=1
+            n/=10
+        return num
+# Solution 2：
+# -*- coding:utf-8 -*-
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        res = 0
+        i = 1
+        while i<=n:
+            a = n//i
+            b = n%i
+            res += (a+8)//10*i + (a%10==1)*(b+1)
+            i*=10
+        return res
+# Solution 3:
+# -*- coding:utf-8 -*-
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        res = 0
+        for i in range(1,n+1):
+            s = str(i)
+            res+=s.count('1')
+        return res
