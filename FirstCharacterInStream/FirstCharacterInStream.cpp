ea11036f9a0023c1f331df2bd513100d45c173d8
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-01 21:14:38 +0800

    Update FirstCharacterInStream.cpp

--- FirstCharacterInStream/FirstCharacterInStream.cpp
+++ FirstCharacterInStream/FirstCharacterInStream.cpp
@@ -1,1 +1,24 @@
-
+class Solution
+{
+public:
+  //Insert one char from stringstream
+    unordered_map<char,int> m;
+    string s;
+    void Insert(char ch)
+    {
+        s += ch;
+        m[ch]++;
+    }
+  //return the first appearence once char in current stringstream
+    char FirstAppearingOnce()
+    {
+        char res = '#';
+        int n = s.size();
+        for (int i=0;i<n;i++)
+        {
+            if (m[s[i]]==1)
+                return s[i];
+        }
+        return res;
+    }
+};
