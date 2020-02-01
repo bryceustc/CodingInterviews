54206cc21d9a965d7ad763660a8e38b9bf758c82
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-01 19:58:22 +0800

    Update FirstNotRepeatingChar.cpp

--- FirstNotRepeatingChar/FirstNotRepeatingChar.cpp
+++ FirstNotRepeatingChar/FirstNotRepeatingChar.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int FirstNotRepeatingChar(string s) {
+        int res = -1;
+        int n = s.size();
+        if (n==0) return res;
+        unordered_map<char,int> m;
+        for (int i=0;i<n;i++)
+        {
+            m[s[i]]++;
+        }
+        for (int i=0;i<n;i++) 
+        {
+            if (m[s[i]]==1)
+            {
+                return i;
+            }
+        }
+        return res;
+    }
+};
