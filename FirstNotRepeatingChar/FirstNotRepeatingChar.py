67cea15b59fe30439710c796adad373d522c1ff3
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-01 19:58:39 +0800

    Update FirstNotRepeatingChar.py

--- FirstNotRepeatingChar/FirstNotRepeatingChar.py
+++ FirstNotRepeatingChar/FirstNotRepeatingChar.py
@@ -1,1 +1,14 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def FirstNotRepeatingChar(self, s):
+        # write code here
+        import collections
+        n = len(s)
+        res = -1
+        if n==0:
+            return res
+        count = collections.Counter(s)
+        for index,ch in enumerate(s):
+            if count[ch]==1:
+                return index
+        return res
