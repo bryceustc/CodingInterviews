1bb312e91b2e930e31d8ba450ef4133e1124476f
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-01 21:15:50 +0800

    Update FirstCharacterInStream.py

--- FirstCharacterInStream/FirstCharacterInStream.py
+++ FirstCharacterInStream/FirstCharacterInStream.py
@@ -1,1 +1,30 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    # 返回对应char
+    def __init__(self):
+        self.s= ""
+    def FirstAppearingOnce(self):
+        # write code here
+        res = "#"
+        for ch in self.s:
+            if self.s.count(ch)==1:
+                return ch
+        return res
+    def Insert(self, char):
+        # write code here
+        self.s+=char
 
+        
+        
+# -*- coding:utf-8 -*-
+class Solution:
+    # 返回对应char
+    def __init__(self):
+        self.s= ""
+    def FirstAppearingOnce(self):
+        # write code here
+        res = list(filter(lambda c: self.s.count(c)==1,self.s))
+        return res[0] if res else "#"
+    def Insert(self, char):
+        # write code here
+        self.s+=char
