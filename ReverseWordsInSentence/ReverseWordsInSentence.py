18ffcec6eebb1e69a683826471ea2fa0be34157d
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-02 23:06:03 +0800

    Update ReverseWordsInSentence.py

--- ReverseWordsInSentence/ReverseWordsInSentence.py
+++ ReverseWordsInSentence/ReverseWordsInSentence.py
@@ -1,1 +1,23 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def ReverseSentence(self, s):
+        # write code here
+        s = list(s)
+        n = len(s)
+        self.Reverse(s,0,n-1)
+        i =0
+        while i<n:
+            while i<n and s[i]==' ':
+                i+=1
+            start = end = i
+            while i<n and s[i]!= ' ':
+                i+=1
+                end+=1
+            self.Reverse(s,start,end-1)
+        s = ''.join(s)
+        return s
+    def Reverse(self,s,start,end):
+        while start<end:
+            s[start],s[end] = s[end],s[start]
+            start+=1
+            end-=1
