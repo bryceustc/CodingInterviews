9c90911361589b888b7ad64f9de995ccff26f18d
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-06 20:47:44 +0800

    Update DeleteNodeInList.cpp

--- DeleteNodeInList/DeleteNodeInList.cpp
+++ DeleteNodeInList/DeleteNodeInList.cpp
@@ -1,1 +1,36 @@
-
+void DeleteNode(ListNode* head,ListNode* ToBeDeleted)
+{
+    if (head == NULL ||ToBeDeleted == NULL)
+        return;
+    // 删除节点不是尾节点
+    if (ToBeDeleted->next != NULL)
+    {
+        ListNode* p = ToBeDeleted->next;
+        ToBeDeleted->val = p->val;
+        ToBeDeleted->next = p->next;
+        delete p;
+        p = NULL;
+    }
+    // 链表只有一个节点，既是头结点，又是尾节点
+    if (head == ToBeDeleted)
+    {
+        delete ToBeDeleted;
+        ToBeDeleted = NULL;
+        head = NULL;
+    }
+    // 链表中有多个节点，删除尾节点
+    else
+    {
+        ListNode* p = head;
+        while(p->next)
+        {
+            if (p->next != ToBeDeleted)
+            {
+                p = p->next;
+            }
+            p->next = NULL;
+            delete ToBeDeleted;
+            ToBeDeleted = NULL;
+        }
+    }
+}
