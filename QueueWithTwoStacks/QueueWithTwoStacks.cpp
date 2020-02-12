460db1bb4b0095d164d8b53f513f0578ec7afe47
bryce <bryce@mail.ustc.edu.cn>
2020-02-12 15:30:56 +0800

    Update QueueWithTwoStacks.cpp

--- QueueWithTwoStacks/QueueWithTwoStacks.cpp
+++ QueueWithTwoStacks/QueueWithTwoStacks.cpp
@@ -1,1 +1,27 @@
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
 
+    int pop() {
+        int temp = 0;
+        if (stack2.empty())
+        {
+            while(!stack1.empty())
+            {
+                temp = stack1.top();
+                stack1.pop();
+                stack2.push(temp);
+            }
+        }
+        int res = stack2.top();
+        stack2.pop();
+        return res;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
