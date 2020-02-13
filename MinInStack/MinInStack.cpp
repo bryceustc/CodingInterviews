0ebfdfc5362c9fc7b5b3c38b5a2c6b6822353d8c
bryce <bryce@mail.ustc.edu.cn>
2020-02-13 15:48:49 +0800

    Update MinInStack.cpp

--- MinInStack/MinInStack.cpp
+++ MinInStack/MinInStack.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    void push(int value) {
+        stack1.push(value);
+        if (stack2.empty() || value < stack2.top())
+        {
+            stack2.push(value);
+        }
+        else
+        {
+            int t = stack2.top();
+            stack2.push(t);
+        }
+    }
+    void pop() {
+        stack1.pop();
+        stack2.pop();
+    }
+    int top() {
+        int a = stack1.top();
+        return a;
+    }
+    int min() {
+        int b = stack2.top();
+        return b;
+    }
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
