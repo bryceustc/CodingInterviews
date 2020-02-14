1697220f35181f00a7e7c22e45fbf863408be0a6
bryce <bryce@mail.ustc.edu.cn>
2020-02-14 15:35:15 +0800

    Update StackPushPopOrder.cpp

--- StackPushPopOrder/StackPushPopOrder.cpp
+++ StackPushPopOrder/StackPushPopOrder.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        int n = pushV.size();
+        int m = popV.size();
+        if (n!=m) return false;
+        stack<int> s;
+        int index = 0;
+        for (int i=0;i<n;i++)
+        {
+            s.push(pushV[i]);
+            while(!s.empty() && s.top()==popV[index])
+            {
+                s.pop();
+                index++;
+            }
+        }
+        return s.empty();
+    }
+};
