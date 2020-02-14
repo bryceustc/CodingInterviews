7b8bce7e0388732fa1a20ab27fac9c760912b26f
bryce <bryce@mail.ustc.edu.cn>
2020-02-14 15:35:39 +0800

    Update StackPushPopOrder.py

--- StackPushPopOrder/StackPushPopOrder.py
+++ StackPushPopOrder/StackPushPopOrder.py
@@ -1,1 +1,18 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def IsPopOrder(self, pushV, popV):
+        # write code here
+        n = len(pushV)
+        m = len(popV)
+        if n!=m:
+            return False
+        s = []
+        index = 0
+        for i in range(n):
+            s.append(pushV[i])
+            while len(s)>0 and s[-1]==popV[index]:
+                s.pop()
+                index+=1
+        if len(s)==0:
+            return True
+        return False
