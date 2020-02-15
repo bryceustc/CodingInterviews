4c05163b0172b8521bf228651eddfccf6d2fe5d5
bryce <bryce@mail.ustc.edu.cn>
2020-02-15 16:28:48 +0800

    Update ConstructBinaryTree.py

--- ConstructBinaryTree/ConstructBinaryTree.py
+++ ConstructBinaryTree/ConstructBinaryTree.py
@@ -33,3 +33,21 @@ class Solution:
         t.left = self.reConstructBinaryTree(left_pre,left_in)
         t.right = self.reConstructBinaryTree(right_pre,right_in)
         return t
+### Solution 2:简洁实现
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, tin):
+        # write code here
+        if not pre or not tin:
+            return None
+        root = TreeNode(pre.pop(0))
+        index = tin.index(root.val)
+        root.left = self.reConstructBinaryTree(pre, tin[:index])
+        root.right = self.reConstructBinaryTree(pre, tin[index + 1:])
+        return root
