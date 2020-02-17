514447cadb23561a3507555ec51ddb27f75066da
bryce <bryce@mail.ustc.edu.cn>
2020-02-17 17:05:46 +0800

    Update MirrorOfBinaryTree.cpp

--- MirrorOfBinaryTree/MirrorOfBinaryTree.cpp
+++ MirrorOfBinaryTree/MirrorOfBinaryTree.cpp
@@ -1,1 +1,28 @@
-
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *root) {
+        if (root ==NULL)
+            return;
+        //交换根节点的左右结点
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right =temp;
+        
+        //递归左子树
+        if (root->left)
+            Mirror(root->left);
+        
+        //递归右子树
+        if (root->right)
+            Mirror(root->right);
+    }
+};
