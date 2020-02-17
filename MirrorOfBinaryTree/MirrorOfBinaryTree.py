9b72e31b19d279bbb681506180e5c865ce9d4a3b
bryce <bryce@mail.ustc.edu.cn>
2020-02-17 17:06:02 +0800

    Update MirrorOfBinaryTree.py

--- MirrorOfBinaryTree/MirrorOfBinaryTree.py
+++ MirrorOfBinaryTree/MirrorOfBinaryTree.py
@@ -1,1 +1,19 @@
-
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回镜像树的根节点
+    def Mirror(self, root):
+        # write code here
+        if root == None:
+            return
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        if root.left:
+            self.Mirror(root.left)
+        if root.right:
+            self.Mirror(root.right)
