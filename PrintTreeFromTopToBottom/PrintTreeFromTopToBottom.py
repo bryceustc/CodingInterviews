2677e4ea3831e51f26884eb11f2c7565c9330171
bryce <bryce@mail.ustc.edu.cn>
2020-02-18 21:51:28 +0800

    Update PrintTreeFromTopToBottom.py

--- PrintTreeFromTopToBottom/PrintTreeFromTopToBottom.py
+++ PrintTreeFromTopToBottom/PrintTreeFromTopToBottom.py
@@ -1,1 +1,35 @@
-
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        res = []
+        if root is None:
+            return res
+        q = []
+        q.append(root)
+        while len(q)>0:
+            temp = q.pop(0)
+            res.append(temp.val)
+            if temp.left:
+                q.append(temp.left)
+            if temp.right:
+                q.append(temp.right)
+        return res
+## collections.deque()
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        # write code here
+        res = []
+        if root is None:
+            return res
+        q = collections.deque()
+        q.append(root)
+        while q:
+            node = q.popleft()
+            res.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return res
