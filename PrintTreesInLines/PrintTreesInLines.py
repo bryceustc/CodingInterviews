e24a36f1548a8b0d1ff011a37021895857c34911
bryce <bryce@mail.ustc.edu.cn>
2020-02-18 22:55:37 +0800

    Update PrintTreesInLines.py

--- PrintTreesInLines/PrintTreesInLines.py
+++ PrintTreesInLines/PrintTreesInLines.py
@@ -1,1 +1,24 @@
-
+### Python Solution 1:
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, root):
+        # write code here
+        res =[]
+        if root is None:
+            return res
+        q = []
+        q.append(root)
+        while q:
+            n = len(q)
+            out = []
+            while n>0:
+                node = q.pop(0)
+                out.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                n-=1
+            res.append(out[:])
+        return res
+### Python Solution 2:
