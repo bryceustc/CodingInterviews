79fa58312cb6edabae7f3458033465908e544832
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 16:48:12 +0800

    Update PrintTreesInZigzag.py

--- PrintTreesInZigzag/PrintTreesInZigzag.py
+++ PrintTreesInZigzag/PrintTreesInZigzag.py
@@ -1,1 +1,32 @@
-
+class Solution:
+    def Print(self, root):
+        # write code here
+        res = []
+        if root is None:
+            return res
+        import collections
+        q = collections.deque()
+        q.append(root)
+        zigzag = True
+        while q:
+            n = len(q)
+            out = []
+            node = None
+            while n>0:
+                if zigzag:
+                    node = q.popleft()
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+                else:
+                    node = q.pop()
+                    if node.right:
+                        q.appendleft(node.right)
+                    if node.left:
+                        q.appendleft(node.left)
+                out.append(node.val)
+                n-=1
+            res.append(out[:])
+            zigzag = not zigzag
+        return res
