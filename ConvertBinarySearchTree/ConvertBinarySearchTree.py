0520eb1e1415239da42a7c306e2d38185da7525e
bryce <bryce@mail.ustc.edu.cn>
2020-02-24 11:17:32 +0800

    Update ConvertBinarySearchTree.py

--- ConvertBinarySearchTree/ConvertBinarySearchTree.py
+++ ConvertBinarySearchTree/ConvertBinarySearchTree.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def Convert(self, root):
+        # write code here
+        if root is None:
+            return None
+        pre = None
+        self.helper(root, pre)
+        res = root
+        while res.left:
+            res = res.left
+        return res
+    def helper(self, cur, pre):
+        if cur is None:
+            return pre
+        pre = self.helper(cur.left, pre)
+        cur.left = pre
+        if pre:
+            pre.right = cur
+        pre = cur
+        return self.helper(cur.right, pre)
