a862839cddda0bfd94ad5c760ee69da499f5270b
bryce <bryce@mail.ustc.edu.cn>
2020-02-25 11:14:30 +0800

    Update NextNodeInBinaryTrees.py

--- NextNodeInBinaryTrees/NextNodeInBinaryTrees.py
+++ NextNodeInBinaryTrees/NextNodeInBinaryTrees.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def GetNext(self, Node):
+        # write code here
+        if Node is None:
+            return None
+        res = None
+        if Node.right:
+            pRight = Node.right
+            while pRight.left:
+                pRight = pRight.left
+            res = pRight
+        elif Node.next:
+            pCur = Node
+            pNext = Node.next
+            while pNext and pNext.right == pCur:
+                pCur = pNext
+                pNext = pNext.next
+            res = pNext
+        return res
