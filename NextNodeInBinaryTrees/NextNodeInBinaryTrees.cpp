2c5592ae450ad279fb7b07141b2a43e15e20e157
bryce <bryce@mail.ustc.edu.cn>
2020-02-25 11:14:15 +0800

    Update NextNodeInBinaryTrees.cpp

--- NextNodeInBinaryTrees/NextNodeInBinaryTrees.cpp
+++ NextNodeInBinaryTrees/NextNodeInBinaryTrees.cpp
@@ -1,1 +1,34 @@
-
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* Node)
+    {
+        if (Node== NULL)
+            return NULL;
+        TreeLinkNode* res = NULL;
+        // 当前结点有右子树，那么它的下一个结点就是它的右子树中最左子结点
+        if (Node->right != NULL)
+        {
+            TreeLinkNode* pRight = Node->right;
+            while(pRight->left!=NULL)
+            {
+                pRight = pRight->left;
+            }
+            res = pRight;
+        }
+        // 当前结点无右子树，则需要找到一个是它父结点的左子树结点的结点
+        else if (Node->next!=NULL)
+        {
+            // 当前结点
+            TreeLinkNode* pCur = Node;
+            // 父节点
+            TreeLinkNode* pNext = Node->next;
+            while( pNext != NULL && pNext->right == pCur)
+            {
+                pCur = pNext;
+                pNext = pNext->next;
+            }
+            res = pNext;
+        }
+        return res;
+    }
+};
