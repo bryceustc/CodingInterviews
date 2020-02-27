13eaf55f88fe929999078a32f9e9a5968e643c01
bryce <bryce@mail.ustc.edu.cn>
2020-02-27 11:37:41 +0800

    Update TreeDepth.cpp

--- TreeDepth/TreeDepth.cpp
+++ TreeDepth/TreeDepth.cpp
@@ -1,1 +1,40 @@
-
+// Solution 1:
+class Solution {
+public:
+    int TreeDepth(TreeNode* root)
+    {
+        if (root == NULL)
+            return 0;
+        int left = TreeDepth(root->left);
+        int right = TreeDepth(root->right);
+        return max(left, right) + 1;
+    }
+};
+// Solution 2:
+class Solution {
+public:
+    int TreeDepth(TreeNode* root)
+    {
+        int depth = 0;
+        if (root == NULL)
+            return depth;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int count = q.size();
+            while(count>0)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+                count--;
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
