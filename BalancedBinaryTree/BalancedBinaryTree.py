d61f25935c2b16d60b84fefb431c9918770d55a8
bryce <bryce@mail.ustc.edu.cn>
2020-02-27 16:51:49 +0800

    Update BalancedBinaryTree.py

--- BalancedBinaryTree/BalancedBinaryTree.py
+++ BalancedBinaryTree/BalancedBinaryTree.py
@@ -1,1 +1,36 @@
-
+# Solution 1:
+class Solution:
+    def IsBalanced_Solution(self, root):
+        # write code here
+        if root is None:
+            return True
+        left = self.TreeDepth(root.left)
+        right = self.TreeDepth(root.right)
+        if left - right > 1 or left - right < -1:
+            return False
+        return self.IsBalanced_Solution(root.left) and self.IsBalanced_Solution(root.right)
+    def TreeDepth(self, root):
+        if root is None:
+            return 0
+        left = self.TreeDepth(root.left)
+        right = self.TreeDepth(root.right)
+        return max(left, right) + 1
+ ## Solution 2:
+class Solution:
+    def __init__(self):
+        self.depth = 0
+    def IsBalanced_Solution(self, root):
+        # write code here
+        if root is None:
+            return True
+        return self.helper(root)[0]
+    def helper(self,root):
+        if root is None:
+            return True, 0
+        leftIsBalanced, left = self.helper(root.left)
+        if not leftIsBalanced:
+            return False, 0
+        rightIsBalanced, right = self.helper(root.right)
+        if not rightIsBalanced:
+            return False, 0
+        return abs(left-right)<=1, max(left,right)+1
