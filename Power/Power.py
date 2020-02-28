6b10fe18dc4b905b0efb3bb2e6c4a33d40cafcb7
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 19:49:15 +0800

    Update Power.py

--- Power/Power.py
+++ Power/Power.py
@@ -1,1 +1,16 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, x, n):
+        # write code here
+        if x==0.0 and n<0:
+            return 0.0
+        res = 1.0
+        if n <0:
+            x=1.0/x
+            n=-n
+        while n:
+            if n&1==1:
+                res*=x
+            x*=x
+            n=n>>1
+        return res
