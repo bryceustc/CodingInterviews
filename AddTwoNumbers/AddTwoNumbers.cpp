e10f81ae95b491bf86c34e7caa460cf427206078
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 17:04:19 +0800

    Update AddTwoNumbers.cpp

--- AddTwoNumbers/AddTwoNumbers.cpp
+++ AddTwoNumbers/AddTwoNumbers.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int Add(int num1, int num2)
+    {
+        while(num2!=0)
+        {
+            int a = num1;
+            num1 = num1^num2;
+            num2 = (unsigned int)(a&num2)<<1;
+        }
+        return num1;
+    }
+};
