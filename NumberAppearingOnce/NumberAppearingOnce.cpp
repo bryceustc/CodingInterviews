77214e803767f6c02b8738bf338a20f55ad37ebe
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 22:35:46 +0800

    Update NumberAppearingOnce.cpp

--- NumberAppearingOnce/NumberAppearingOnce.cpp
+++ NumberAppearingOnce/NumberAppearingOnce.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> res (2,0);
+        int number = 0;
+        int n = nums.size();
+        for (int i=0;i<n;i++)
+        {
+            number ^= nums[i];
+        }
+        // lowbit1 : 是二进制表达式中最低位的 1 所对应的值
+        int lowbit1 = number&(-number);
+        for (int num : nums)
+        {
+            if ((num & lowbit1) == lowbit1)
+            {
+                res[0] ^= num;
+            }
+            else
+            {
+                res[1]^= num;
+            }
+        }
+        return res;
+    }
+};
