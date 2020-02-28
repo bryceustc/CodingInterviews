b2aa47e230a82b829b0b9f7501d4791ba628830e
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 17:04:35 +0800

    Update AddTwoNumbers.py

--- AddTwoNumbers/AddTwoNumbers.py
+++ AddTwoNumbers/AddTwoNumbers.py
@@ -1,1 +1,11 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def Add(self, num1, num2):
+        # write code here
+        MAX = 0x7fffffff
+        mask = 0xffffffff
+        while num2 != 0:
+            num1, num2 = (num1 ^ num2), ((num1 & num2) << 1)
+            num1 = num1 & mask
+            num2 = num2 & mask
+        return num1 if num1 <= MAX else ~(num1 ^ mask)
