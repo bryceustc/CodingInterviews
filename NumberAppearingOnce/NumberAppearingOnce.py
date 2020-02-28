d4a759e81cdb7ccdee873d7c4d00a8e524c63e2b
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 22:36:03 +0800

    Update NumberAppearingOnce.py

--- NumberAppearingOnce/NumberAppearingOnce.py
+++ NumberAppearingOnce/NumberAppearingOnce.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def singleNumbers(self, nums: List[int]) -> List[int]:
+        number = 0
+        res = [0,0]
+        for num in nums:
+            number ^= num
+        pos = 1
+        while (number&pos) == 0:
+            pos <<=1
+        for num in nums:
+            if (num&pos) == pos:
+                res[0] ^= num
+            else:
+                res[1] ^= num
+        return res
