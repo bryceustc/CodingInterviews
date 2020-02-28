6af781f6be272415d1f406e136fcc4e73b0eaad7
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 15:35:45 +0800

    Update NumberOf1InBinary.cpp

--- NumberOf1InBinary/NumberOf1InBinary.cpp
+++ NumberOf1InBinary/NumberOf1InBinary.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int count = 0;
+         while(n)
+         {
+             count++;
+             n = (n-1)&n;
+         }
+         return count;
+     }
+};
