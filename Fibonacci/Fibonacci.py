eb53ec5ed439e523c613dc1ab8f2698292760d61
bryce <bryce@mail.ustc.edu.cn>
2020-02-29 22:05:03 +0800

    Update Fibonacci.py

--- Fibonacci/Fibonacci.py
+++ Fibonacci/Fibonacci.py
@@ -1,1 +1,14 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def Fibonacci(self, n):
+        # write code here
+        if n<2:
+            return n
+        fn_one = 1
+        fn_two = 0
+        fn = 0
+        for i in range(2,n+1):
+            fn = fn_one + fn_two
+            fn_two = fn_one
+            fn_one = fn
+        return fn
