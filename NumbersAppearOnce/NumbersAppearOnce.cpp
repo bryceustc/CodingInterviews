18aa43dfff9f966092345c32ded8d842ec53dd13
bryce <bryce@mail.ustc.edu.cn>
2020-02-29 17:48:46 +0800

    Update NumbersAppearOnce.cpp

--- NumbersAppearOnce/NumbersAppearOnce.cpp
+++ NumbersAppearOnce/NumbersAppearOnce.cpp
@@ -1,55 +1,18 @@
-// C++ Solution 1:
 class Solution {
 public:
-    int GetNumberOfK(vector<int> nums ,int k) {
-        int res = 0;
-        if (nums.empty())
-            return 0;
-        int n = nums.size();
-        for (int i=0;i<n;i++)
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++)
         {
-            if (nums[i]==k)
-                res++;
+            int count = 0;
+            for (auto n : nums)
+            {
+                if ((1 << i ) & n) 
+                    count++;
+            }
+            if (count % 3) 
+                ans += (1 << i);
         }
-        return res;
-    }
-};
-
-// C++ Solution 2:
-class Solution {
-public:
-    int GetNumberOfK(vector<int> nums ,int k) {
-        int res = 0;
-        if (nums.empty())
-            return 0;
-        int n = nums.size();
-        int start = 0;
-        int end = n;
-        while (end > start)
-        {
-            int mid = start + (end-start)/2;
-            if (nums[mid]==k)
-                end=mid;
-            if (nums[mid]<k)
-                start = mid+1;
-            if (nums[mid]>k)
-                end = mid;
-        }
-        int temp1 = start;
-        start = 0;
-        end = n;
-        while (end > start)
-        {
-            int mid = start + (end-start)/2;
-            if (nums[mid]==k)
-                start=mid+1;
-            if (nums[mid]<k)
-                start = mid+1;
-            if (nums[mid]>k)
-                end = mid;
-        }
-        int temp2 = start;
-        res = temp2-temp1;
-        return res;
+        return ans;
     }
 };
