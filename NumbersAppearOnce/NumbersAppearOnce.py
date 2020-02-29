c0b6cc17a4fe1791ede62ad021877515a1e7798f
bryce <bryce@mail.ustc.edu.cn>
2020-02-29 17:52:28 +0800

    Update NumbersAppearOnce.py

--- NumbersAppearOnce/NumbersAppearOnce.py
+++ NumbersAppearOnce/NumbersAppearOnce.py
@@ -1,46 +1,11 @@
-### Python Solution 1:
-# -*- coding:utf-8 -*-
 class Solution:
-    def GetNumberOfK(self, nums, k):
-        # write code here
+    def singleNumber(self, nums: List[int]) -> int:
         res = 0
-        n = len(nums)
-        if n==0:
-            return 0
-        for i in range(n):
-            if nums[i]==k:
-                res+=1
-        return res
-### Python Solution 2:
-# -*- coding:utf-8 -*-
-class Solution:
-    def GetNumberOfK(self, nums, k):
-        # write code here
-        res = 0
-        n = len(nums)
-        if n==0:
-            return 0
-        start = 0
-        end = n
-        while end > start:
-            mid = start + (end-start)//2
-            if nums[mid]==k:
-                end = mid
-            if nums[mid] < k:
-                start = mid+1
-            if nums[mid] > k:
-                end = mid
-        temp1 = start ## 一次二分查找寻找左侧边界
-        start = 0
-        end = n
-        while end > start:
-            mid = start + (end - start)//2
-            if nums[mid] == k:
-                start = mid + 1
-            if nums[mid] < k:
-                start = mid + 1
-            if nums[mid] > k:
-                end = mid
-        temp2 = end - 1  ## 一次二分查找寻找右侧边界，注意右侧边界要减一
-        res = temp2-temp1+1
+        for i in range(0,32):
+            count = 0
+            for num in nums:
+                if (num&(1<<i)):
+                    count+=1
+            if (count%3):
+                res += 1<<i
         return res
