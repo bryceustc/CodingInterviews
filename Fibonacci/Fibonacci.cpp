d29d800dfa376a7b7f1144de68a3990251bb8969
bryce <bryce@mail.ustc.edu.cn>
2020-02-29 22:04:40 +0800

    Update Fibonacci.cpp

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int Fibonacci(int n) {
+        if (n<2)
+            return n;
+        int fn_one = 1;
+        int fn_two = 0;
+        int fn = 0;
+        for (int i=2;i<=n;i++)
+        {
+            fn = fn_one + fn_two;
+            fn_two = fn_one;
+            fn_one = fn;
+        }
+        return fn;
+    }
+};
