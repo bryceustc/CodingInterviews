8535603c8be1bdda82b2fbf912064e6316365750
bryce <bryce@mail.ustc.edu.cn>
2020-02-29 22:33:40 +0800

    Update JumpFloorII.cpp

--- JumpFloorII/JumpFloorII.cpp
+++ JumpFloorII/JumpFloorII.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int jumpFloorII(int n) {
+        if (n==0)
+            return 0;
+        int res = 1;
+        for (int i=1;i<n;i++)
+        {
+            res*=2;
+        }
+        return res;
+    }
+};
