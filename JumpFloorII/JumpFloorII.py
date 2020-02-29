292b12749c2d4e9ae823cf18d8b19ff1184b3f51
bryce <bryce@mail.ustc.edu.cn>
2020-02-29 22:33:57 +0800

    Update JumpFloorII.py

--- JumpFloorII/JumpFloorII.py
+++ JumpFloorII/JumpFloorII.py
@@ -1,1 +1,10 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def jumpFloorII(self, n):
+        # write code here
+        if n==0:
+            return 0
+        res = 1
+        for i in range(1,n):
+            res *= 2
+        return res
