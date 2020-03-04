2c3c257371af0fb61bcadaf5a0f577fe7209d08e
bryce <bryce@mail.ustc.edu.cn>
2020-03-04 21:44:18 +0800

    Update PrintNumbers.py

--- PrintNumbers/PrintNumbers.py
+++ PrintNumbers/PrintNumbers.py
@@ -1,1 +1,30 @@
-
+class Solution:
+    def __init__(self):
+        self.res = []
+    def printNumbers(self, n: int) -> List[int]:
+        self.res = []
+        if n<=0:
+            return self.res
+        numer = ['0' for _ in range(n)]
+        self.helper(0, n, numer)
+        return self.res;
+    def helper(self, index, n, numer):
+        if index == n:
+            self.saveNumber(numer)
+            return
+        for i in range(10):
+            numer[index] = str(i)
+            self.helper(index+1, n, numer)
+    def saveNumber(self, numer):
+        index = 0
+        s = ""
+        while (index<len(numer)):
+            if (numer[index]!='0'):
+                break
+            index+=1
+        while (index<len(numer)):
+            s+=numer[index]
+            index+=1
+        if s:
+            num = int(s)
+            self.res.append(num)
