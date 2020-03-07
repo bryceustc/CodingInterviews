a6847e04f2530d464456098cc773d0ac718ac7e8
bryce <bryce@mail.ustc.edu.cn>
2020-03-07 17:35:00 +0800

    Update TranslateNum.cpp

--- TranslateNum/TranslateNum.cpp
+++ TranslateNum/TranslateNum.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int translateNum(int num) {
+        string s = to_string(num);
+        int n = s.size();
+        vector<int> dp(n+1, 0); // dp[i]表示前i个数字的翻译种数，注意dp[i]对应的数字是s[i-1]
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i =2;i<n+1;i++)
+        {
+            if (s[i-2] == '0' || s.substr(i-2, 2)>"25")
+            {
+                dp[i] = dp[i-1];
+            }
+            else
+            {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
